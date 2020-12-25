1ace2cdbbf808d4d2fbdc29a2e4af12e532f9bcb
ntkjer <ntkjer@gmail.com>
2020-12-25 18:42:14 -0500

    removed hold mod for spacebar

--- keymap.c
+++ keymap.c
@@ -49,14 +49,13 @@ h_ctrl*| Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |  K   |   L  |  
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Lower| Ctrl | Alt  | GUI  |    Space    |   Space     | Left | Down |  Up  |Right |
      * `-----------------------------------------------------------------------------------'
-                                      hold_shift   hold_shift
 */
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-  LOWER, KC_LCTL, KC_LALT, KC_LGUI, LSFT_T(KC_SPC),  LSFT_T(KC_SPC),  LSFT_T(KC_SPC),  LSFT_T(KC_SPC),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  
+  LOWER, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  
 ),
 
 /* Colemak
